53db5322d9887277f7f15f25230cfbeedcf6f281
JUNIORPRUEVA <jrdigitalrd@gmail.com>
2026-03-16 21:43:48 -0400

    mmnmn

--- backend/src/modules/bot-configuration/bot-configuration.module.ts
+++ backend/src/modules/bot-configuration/bot-configuration.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { PersistenceModule } from '../../common/persistence/persistence.module';
+import { BillingModule } from '../billing/billing.module';
 import { ChannelsModule } from '../channels/channels.module';
 import { BotConfigurationController } from './controllers/bot-configuration.controller';
 import { BotConfigurationEntity } from './entities/bot-configuration.entity';
@@ -10,6 +11,7 @@ import { BotConfigurationService } from './services/bot-configuration.service';
 @Module({
   imports: [
     PersistenceModule,
+    BillingModule,
     ChannelsModule,
     TypeOrmModule.forFeature([BotConfigurationEntity]),
   ],
